daa3348e2e9a91ab67af63c5cc2bba78f4bd046e
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-07-30 22:25:30 -0500

    updated component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <h1>Hi there</h1>
+    <h1>{{ makeUpper( monkey ) }}</h1>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      monkey: "ssssssSS"
+    }
+  },
+  methods: {
+    makeUpper: function(value) { return value.toUpperCase()}
+  }
   
 }
 </script>
